7491670d6530a30b221eed9564713324bca93ae9
Trollope91 <ljtdev91@gmail.com>
2023-07-18 10:12:14 +0000

    Add user request loop

--- run.py
+++ run.py
@@ -14,21 +14,28 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
     """
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, seperated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Enter your data here")
+        data_str = input("Enter your data here: ")
 
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
+
 
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
-    Raise Valueerror if strings cannot be converted into int,
+    Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
     try:
@@ -38,9 +45,10 @@ def validate_data(values):
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
-        print(f"Invalid data: {e}, please try again.\n")   
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
 
-  
-get_sales_data()    
+    return True
 
 
+data = get_sales_data()
